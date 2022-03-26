9fd5270db927aebf2a352bda2da27773478d35d4
Romina Silvero <rominasilveroiq@gmail.com>
2022-03-25 23:16:40 -0300

    Termino mi primer proyecto

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//acceso al dom
+document.write('<p>Esta es nuestra primer clase de JavaScritp!!</p>')
+
+//variable tipo objeto
+
+const persona = {
+    nombre : 'maria',
+    dni: 123456789,
+    provincia:'mendoza',
+    pais: 'argentina',
+}
+
+//imprimir la variable de tipo objecto
+console.log(persona),
+console.log('quiero los datos de la persona con dni:' + persona.dni)
---
+++ js/operadores.js
@@ -0,0 +1,25 @@
+let edad = 18;
+let dinero = false;
+
+if(edad == 18 && dinero == true){
+console.log('genial! podes comprar bebidas!!')
+} else{
+    console.log('lo siento, sera en otra oportunidad')
+}
+
+edad = 17
+
+if(edad < 18){
+    console.log('lo siento, no podes ingresa a la fiesta')
+}
+
+let comprar = true
+let asientos = false
+
+if(comprar){
+    console.log('bienvenidos a la empresa de pasajes')
+} else if(asientos){
+    console.log('lo sentimos, este vuelo viaja completo y no tenemos haciento')
+}else{
+    console.log('esta invitado a viajar cuando quiera ')
+}
